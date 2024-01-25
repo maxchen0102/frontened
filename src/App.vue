168d5157caba3605b39d6f5258380c5ef939ea85
Chris Chen <chris.chen@ecpay.com.tw>
2024-01-25 16:02:09 +0800

    +2

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,8 @@
     </div>
   </nav>
   <P>我是整個網站的模板componnet</P>
+  <P>我是整個網站的模板componnet</P>
+  <P>我是整個網站的模板componnet</P>
   <!--component render place -->
   <router-view></router-view>
   <router-view name="menu"></router-view>
