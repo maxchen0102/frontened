c8155a6376f9a0df05fcc03e7d15d148719f4f06
Chris Chen <chris.chen@ecpay.com.tw>
2024-01-26 11:28:41 +0800

    新增 新增商品按鈕

--- src/components/pages/products.vue
+++ src/components/pages/products.vue
@@ -1,29 +1,75 @@
 <template>
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">id</th>
-        <th scope="col">category</th>
-        <th scope="col">num</th>
-        <th scope="col">price</th>
-        <th scope="col">origin_price</th>
-        <th scope="col">title</th>
-        <th scope="col">unit</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in products">
-        <td>{{ item.id }}</td>
-        <td>{{ item.category }}</td>
+  <div>
+    <div>
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#productModal"
+      >
+        建立新的產品
+      </button>
+    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">category</th>
+          <th scope="col">num</th>
+          <th scope="col">price</th>
+          <th scope="col">origin_price</th>
+          <th scope="col">title</th>
+          <th scope="col">unit</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in products">
+          <td>{{ item.id }}</td>
+          <td>{{ item.category }}</td>
 
-        <td>{{ item.num }}</td>
-        <td>{{ item.price }}</td>
-        <td>{{ item.origin_price }}</td>
-        <td>{{ item.title }}</td>
-        <td>{{ item.unit }}</td>
-      </tr>
-    </tbody>
-  </table>
+          <td>{{ item.num }}</td>
+          <td>{{ item.price }}</td>
+          <td>{{ item.origin_price }}</td>
+          <td>{{ item.title }}</td>
+          <td>{{ item.unit }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="productModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
