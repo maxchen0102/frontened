ea8b06669bbf29e6885b7c22f61dc6aa0688b584
Chris Chen <chris.chen@ecpay.com.tw>
2024-02-06 16:44:16 +0800

    no message

--- src/components/datapage/index.vue
+++ src/components/datapage/index.vue
@@ -1,55 +1,54 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-4"></div>
-      <div class="col-4">
-        <h1>data testing</h1>
-        <div class="input-group input-group-lg">
-          <div class="input-group-prepend">
-            <span class="input-group-text" id="inputGroup-sizing-lg"
-              >Input</span
-            >
-          </div>
-          <input
-            type="text"
-            class="form-control"
-            aria-label="Large"
-            aria-describedby="inputGroup-sizing-sm"
-            v-model="score"
-          />
-          <input
-            type="text"
-            class="form-control"
-            aria-label="Large"
-            aria-describedby="inputGroup-sizing-sm"
-            v-model="score2"
-          />
-        </div>
-      </div>
-      <div class="col-4">
-        <button @click="cal()" type="button" class="btn btn-success">
-          結果
-        </button>
-        {{ score }}
-        {{ total }}
-      </div>
-    </div>
-  </div>
+  <button @click="change(flag)" type="button" class="btn btn-danger">
+    改變
+  </button>
+  <ul v-if="flag">
+    <li v-for="item in arr">{{ item }}</li>
+  </ul>
+  外層{{ msg }}
+
+  <innerpage
+    v-bind:innerMsg="msg"
+    v-bind:innerArr="arr"
+    v-bind:innerBooks="books"
+  ></innerpage>
 </template>
 
 <script>
+import innerpage from "./innerpage.vue";
 export default {
+  components: {
+    innerpage,
+  },
   data() {
     return {
       a: 1234,
       score: 0,
       score2: 0,
       total: 0,
+      flag: true,
+      arr: ["chris", "max", "bob"],
+      msg: "hello",
+      books: [
+        {
+          id: 123,
+          name: "magic",
+          author: "chris",
+        },
+        {
+          id: 2,
+          name: "economic",
+          author: "max",
+        },
+      ],
     };
   },
   methods: {
     cal(score, score2) {
       this.total = score + score2;
+    },
+    change(flag) {
+      this.flag = !flag;
     },
   },
 };
---
+++ src/components/datapage/innerpage.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>inner</h1>
+  內層{{ innerMsg }} <br />
+  內層 {{ innerArr }} <br />
+  <ul v-for="book in innerBooks">
+    <li>id = {{ book.id }}</li>
+    <li>name={{ book.name }}</li>
+    <li>author={{ book.author }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    innerMsg: String,
+    innerBooks: {
+      type: [String, Object],
+    },
+    innerArr: {
+      tpye: [String, Array],
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
