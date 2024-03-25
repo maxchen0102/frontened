e742708e3d8d8a0e7459dda25d340c045b0683a1
Chris Chen <chris.chen@ecpay.com.tw>
2024-03-25 10:01:29 +0800

    完成navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,22 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div>
+    <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+     
       <div class="container-fluid">
         <a class="navbar-brand" href="home">首頁</a>
         <button
           class="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
+          @click="toggleNavbar"
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
+        <div :class="{'collapse': !navbarOpen, 'navbar-collapse': true}" id="navbarNav">
+          <!-- Navbar content -->
+                <ul class="navbar-nav">
             <li class="nav-item">
               <!---->
               <router-link class="nav-link active" to="/home">home</router-link>
@@ -55,8 +56,32 @@
               >
             </li>
           </ul>
+        
         </div>
       </div>
     </nav>
   </div>
+
+
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      navbarOpen: true // Initially open
+    };
+  },
+  methods: {
+    toggleNavbar() {
+      this.navbarOpen = !this.navbarOpen;
+    }
+  }
+};
+</script>
+
+<style>
+/* Add any custom styles here */
+
+
+</style>
